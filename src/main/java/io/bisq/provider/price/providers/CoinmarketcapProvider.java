7f9f76c90192313718da69f34f149872a0023362
Chris Beams <chris@beams.io>
2017-08-22 20:22:39 +0200

    Configure wildcard imports and reformat

--- src/main/java/io/bisq/provider/price/providers/CoinmarketcapProvider.java
+++ src/main/java/io/bisq/provider/price/providers/CoinmarketcapProvider.java
@@ -28,7 +28,11 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.time.Instant;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static java.lang.Double.parseDouble;
